5e2ded45c04c1b42458dbcec82767157b793b483
nehasharma2998 <nehasharma29@outlook.com>
2019-02-05 12:14:18 -0500

    discussion 4

---
+++ Discussion 4/Sandbox.java
@@ -0,0 +1,160 @@
+/**
+ * Write a description of class Sandbox here.
+ *
+ * @author Neha Sharma
+ * @version 02/05/19
+ */
+public class Sandbox
+{ 
+    //problem 1
+    //list the standard things we create for every class implemented
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    //problem 2
+    //Create a method that will receive two integer parameters. Print the sum and the product of the integers
+    //sample output
+    //The sum of 22 and 10 equals:      32
+    
+    
+    
+    
+    //Then compare and print the equivalence of the original two integers. 
+    //The product of 22 and 10 equals:  220
+    
+    
+    
+    //Return the result of the comparison.
+    //It is false that 22 equals 10
+    
+    
+    
+  
+    
+    
+    //problem 3
+    //Create a method that will receive two decimal parameters. 
+    
+    
+    //Print the sum and the division of the decimal numbers, dividing the first number by the second. 
+    //The sum of 44.156 and 8.3 equals:      52.456
+    //The division of 44.156 by 8.3 equals:  5.32
+    
+    
+    
+    //Then compare and print whether the first of the original two decimal numbers is greater than the second. Return the result of the division.
+    //It is true that 44.156 is greater than 8.3
+    
+    
+    
+    
+    
+    
+    
+    //problem 4
+    //Create a method that will receive two numeric parameters (a and b)
+    
+    //Determine whether a and b are greater than zero. 
+    
+    
+    //If either is less than zero, print a message stating which values are not greater than zero
+    
+    
+    
+    
+    
+    
+    //problem 5
+    //given this code,
+    //state which of the if's, else if's and else's connect to each other. 
+    //Then step through code, state the output created, and the final value of each variable.
+    public void problem5(){
+        int  x = 7, y = 3, z = 12;
+
+        if (z != x) {
+          y  +=  x;
+          System.out.println("Print #1");
+        }
+        //output:
+        //final values
+        //x:
+        //y:
+        //z:
+        else if (x <= z || true) {
+          z = x / y;
+          System.out.println("Print #2");
+        }
+        //output:
+        //final values
+        //x:
+        //y:
+        //z:
+        else if (z >= (x / y)) {
+          System.out.println("Print #3");
+        }
+        //output:
+        //final values
+        //x:
+        //y:
+        //z:
+        else {
+          y -= z;
+          System.out.println("Print #4");
+        }
+        //output:
+        //final values
+        //x:
+        //y:
+        //z:
+        if (x  >=  z) {
+          System.out.println("Print #5");
+        }
+        //output:
+        //final values
+        //x:
+        //y:
+        //z:
+        else if (y != 0 && (x > 4 || x == 0)) {
+          x = x - y;
+          System.out.println("Print #6");
+        }
+        //output:
+        //final values
+        //x:
+        //y:
+        //z:
+        else {
+          z = x + y;
+          System.out.println("Print #7");
+        }
+        //output:
+        //final values
+        //x:
+        //y:
+        //z:
+    }
+    
+    //problem 6 (not sure we'll get to this)
+    //Within your method solution from the previous question, create a break point at the line: if (z != x) { 
+    //Execute the method and use the debugger to step through each statement of the method. 
+    //As you step through the program, note the change to the variable values and the information printed to the terminal at each statement. 
+    //State the resulting value for each variable and any printed output.
+    
+    
+    
+     
+    
+    
+    
+    
+    
+    
+    
+}
